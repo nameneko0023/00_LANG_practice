be15fe4d932605615551cfc0e228d790b14350fd
tomoya <nameneko1123@gmai.com>
2022-09-25 13:05:21 +0900

    更新

---
+++ 06_BAT/初期設定用/Set_Path.bat
@@ -0,0 +1,22 @@
+@echo off
+rem 7Zipのパスを通す
+
+:: ==変数==
+
+rem アプリ名
+set APP_NAME=7zip.exe
+
+::カレントディレクトリ
+set CURRENT_DIR=%CD%
+
+
+rem ==関数==
+
+echo %CURRENT_DIR%
+
+rem ==メイン処理==
+
+pause
+exit /b
+
+
